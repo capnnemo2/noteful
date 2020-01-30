687fb96f1814837cd405a8671c0caf8d7e5031e7
Ben Hernandez <benjammin234@gmail.com>
2020-01-30 10:07:23 -0800

    Displays notes in folders

--- src/Folder/Folder.js
+++ src/Folder/Folder.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import dummyStore from '../dummyStore';
 
 export default class Folder extends React.Component {
 
     render() {
 
-        const folder = dummyStore.folders.find(f => f.id === this.props.match.params.folderId)
+        const folder = dummyStore.folders.find(f => f.id === this.props.match.params.folderId);
+
+        const notes = dummyStore.notes.filter(n => n.folderId === this.props.match.params.folderId);
 
         return (
             <div className='Folder'>
                 <h2>{folder.name}</h2>
+
+                <ul>
+                    {notes.map(note =>
+                        <li key={note.id} className=''>
+                            <Link to={`/note/${note.id}`} className=''>
+                                {note.name}
+                            </Link>
+                            <button type='button'>Delete</button>
+                        </li>
+                    )}
+                </ul>
+
             </div>
         )
     }
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -2,11 +2,11 @@ import React from 'react';
 import dummyStore from '../dummyStore';
 
 export default function Note(props) {
-    // const note = dummyStore.find(n => n.id === props.match.params.noteId)
+    const note = dummyStore.notes.find(n => n.id === props.match.params.noteId)
     return (
         <div className='Note'>
-            {/* <h2>{note.name}</h2>
-            <p>{note.content}</p> */}
+            <h2>{note.name}</h2>
+            <p>{note.content}</p>
         </div>
     )
 }
