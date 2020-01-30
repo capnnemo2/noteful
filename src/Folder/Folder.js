cb827c51e2641fd5f84163399e8e50d8ac6ed169
Ben Hernandez <benjammin234@gmail.com>
2020-01-30 09:54:05 -0800

    Fixes Folder to navigate to correct folder

--- src/Folder/Folder.js
+++ src/Folder/Folder.js
@@ -5,11 +5,11 @@ export default class Folder extends React.Component {
 
     render() {
 
-        // const folder = dummyStore.find(f => f.id === this.props.match.params.folderId)
+        const folder = dummyStore.folders.find(f => f.id === this.props.match.params.folderId)
 
         return (
             <div className='Folder'>
-                {/* <h2>{folder.name}</h2> */}
+                <h2>{folder.name}</h2>
             </div>
         )
     }
