feaee5ac96db12032003f875612234e40efcab1b
Ben Hernandez <benjammin234@gmail.com>
2020-02-02 14:00:07 -0800

    Broke Note functionality trying to use context

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,24 +1,37 @@
 import React from "react";
-import dummyStore from "../dummyStore";
+import NotefulContext from "../NotefulContext";
 import "./Note.css";
 
-export default function Note(props) {
-  const note = dummyStore.notes.find(n => n.id === props.match.params.noteId);
-  const noteFolder = dummyStore.folders.find(f => f.id === note.folderId);
-  return (
-    <div className="Note wrapper">
-      <div className="Note__nav">
-        <h3>{noteFolder.name}</h3>
-        <button type="button" onClick={() => props.history.goBack()}>
-          Back
-        </button>
-      </div>
+export default class Note extends React.Component {
+  static defaultProps = {
+    history: {
+      goBack: () => {}
+    },
+    match: {
+      params: {}
+    }
+  };
+  static contextType = NotefulContext;
+  render() {
+    const { notes = [] } = this.context;
+    const { folders = [] } = this.context;
+    const note = notes.find(n => n.id === this.props.match.params.noteId);
+    const noteFolder = folders.find(f => f.id === note.folderId);
+    return (
+      <div className="Note wrapper">
+        <div className="Note__nav">
+          <h3>{noteFolder.name}</h3>
+          <button type="button" onClick={() => this.props.history.goBack()}>
+            Back
+          </button>
+        </div>
 
-      <div className="Note__content">
-        <h2>{note.name}</h2>
-        <p>{note.content}</p>
-        <button type="button">Delete</button>
+        <div className="Note__content">
+          <h2>{note.name}</h2>
+          <p>{note.content}</p>
+          <button type="button">Delete</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
