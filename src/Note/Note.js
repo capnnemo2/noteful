9ea2d8490f20dc9dc84b65b57332105b66481856
Ben Hernandez <benjammin234@gmail.com>
2020-02-02 14:09:33 -0800

    Still does not work

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -11,12 +11,17 @@ export default class Note extends React.Component {
       params: {}
     }
   };
+
+  findNote = (notes = [], noteId) => notes.find(note => note.id === noteId);
+
   static contextType = NotefulContext;
   render() {
     const { notes = [] } = this.context;
     const { folders = [] } = this.context;
-    const note = notes.find(n => n.id === this.props.match.params.noteId);
-    const noteFolder = folders.find(f => f.id === note.folderId);
+    const { noteId } = this.props.match.params;
+    const { folderId } = this.props.match.params;
+    const note = notes.find(note => note.id === noteId);
+    const noteFolder = folders.find(f => f.id === folderId);
     return (
       <div className="Note wrapper">
         <div className="Note__nav">
