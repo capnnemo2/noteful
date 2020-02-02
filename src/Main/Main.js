41bd16d2131229f4e956e1e4444ddba2bc66db76
Ben Hernandez <benjammin234@gmail.com>
2020-02-02 13:38:27 -0800

    Trying to get delete functionality

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -5,6 +5,31 @@ import "./Main.css";
 
 export default class Main extends React.Component {
   static contextType = NotefulContext;
+
+  handleDeleteNote = e => {
+    e.preventDefault();
+    const noteId = this.props.id;
+    const url = "http://localhost:9090/notes/";
+    console.log(noteId);
+    fetch(url + `${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then(() => {
+        this.context.deleteNote(noteId);
+      })
+      .catch(error => {
+        console.error({ error });
+      });
+  };
   render() {
     const { notes = [] } = this.context;
     return (
@@ -15,7 +40,9 @@ export default class Main extends React.Component {
               <Link to={`/note/${note.id}`} className="Main__link">
                 {note.name}
               </Link>
-              <button type="button">Delete</button>
+              <button type="button" onClick={this.handleDeleteNote}>
+                Delete
+              </button>
             </li>
           ))}
         </ul>
