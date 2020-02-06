0a110e454eafee10cb15ec17645da8e22a97352c
Ben Hernandez <benjammin234@gmail.com>
2020-02-06 08:04:49 -0800

    Completes AddFolder form

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,13 +1,31 @@
 import React from "react";
 
 export default class AddFolder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      folderName: ""
+    };
+  }
+
+  updateFolderName(name) {
+    this.setState({ folderName: { value: name } });
+  }
+
   handleSubmit = e => {
     e.preventDefault();
+    const { name } = this.state;
+    console.log(name);
   };
   render() {
     return (
       <form className="AddFolder" onSubmit={e => this.handleSubmit(e)}>
-        <input type="text" name="name" id="name" defaultValue="New Folder" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={e => this.updateFolderName(e.target.value)}
+        />
         <button type="submit" className="AddFolder__btn">
           Add
         </button>
