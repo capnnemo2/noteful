ad00e8a769ffaab34441c50af9af6f2c6cd8d094
Ben Hernandez <benjammin234@gmail.com>
2020-03-14 17:01:55 -0700

    Updates smoke test on app.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT_FOLDERS: `http://localhost:8000/api/folders`,
-  API_ENDPOINT_NOTES: `http://localhost:8000/api/notes`
+  API_ENDPOINT_FOLDERS: `https://lit-springs-75612.herokuapp.com/api/folders`,
+  API_ENDPOINT_NOTES: `https://lit-springs-75612.herokuapp.com/api/notes`
   // API_KEY: process.env.API_TOKEN
 };
